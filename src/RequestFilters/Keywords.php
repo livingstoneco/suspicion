a58dbd68ce4703a4e252ab09bf4ec9b6298688b3
Neil Livingstone <nlivingstone@users.noreply.github.com>
2024-01-29 12:10:57 -0500

    Add new keywords
    * telegram
    * bitcoin
    * cryptocurrency

--- src/RequestFilters/Keywords.php
+++ src/RequestFilters/Keywords.php
@@ -20,8 +20,8 @@ class Keywords
         // Loop through request parameters to determine if they contain banned keywords
         foreach ($request->all() as $input) {
             foreach ($this->keywords as $keyword) {
-                if (preg_match("/\b" . preg_quote($keyword) . '\s/mi', $input)) {
-                    $this->logRequest($request);
+                if (preg_match("/\b" . preg_quote($keyword) . '\b/mi', $input)) {
+                    $this->logRequest($request, $keyword);
                     abort('422', config('suspicion.error_message'));
                 }
             }
@@ -33,11 +33,11 @@ class Keywords
     // Return array of banned keywords
     private function getKeywords()
     {
-        return ['wordpress', 'woocommerce', 'joomla', 'spa', 'affiliate program', 'prestashop', 'homeopathic', 'incentives', 'deflationary', 'recompound', 'crypto', 'ROI', 'binance', 'reinvestment', 'token', 'financial', 'B2B', 'ecommerce store', 'webmasters', 'nymphomania', 'opencart', 'forex', 'magento', 'bobinternetmarketing', 'business contacts', 'pornography', 'porn', 'capterra', 'dbms', 'whatsapp', 'search engine optimization', 'search results', 'google search', 'guest post', 'social media', 'website traffic', 'no-reply', 'noreply', 'seo', 'smm', 'gay', 'homo', 'homosexual', 'sexual', 'sex', 'bombay', 'mumbai', 'india', 'dating', 'romance', 'babes', 'meet singles', 'earn income', 'earn money', 'xxx', 'marketing', 'blogger', 'article placement', 'service expiration', 'spam', 'medicine', 'human growth hormone', 'life insurance', 'lose weight', 'medicine', 'no medical exams', 'online pharmacy', 'removes wrinkles', 'reverses aging', 'stop snoring', 'valium', 'viagra', 'vicodin', 'weight loss', 'xanax', 'casino', 'paid members', 'lead generation', 'disease', 'romania', 'prostitute', 'mailing list', 'php', 'asp', 'cdb', 'thc', 'delta 8', 'cialis', 'hair loss', 'data recovery', 'nudes', 'hardcore', 'abc', 'url=', 'captcha', 'product key', 'homeopathic', 'nausea', 'remedy', 'remedies', 'online chat', 'chat online', 'sweatheart', 'pakistan', 'href', 'cock', 'suck', 'partnerships', 'lolita', 'fuck', 'loli', 'foto', 'cp', 'pthc', 'clinical', 'cbd', 'cbn', 'pussy', 'masterbate', 'masterbating', 'tits', 'adsense', 'tutorial', 'platform', 'payouts', 'algorithms', 'machine learning', 'machine-learning', 'artificial intelligence', 'ai', 'revenue', 'robot', 'automate', 'backlinks', 'optimization', 'reputation', 'freelancer', 'public relations', 'advertising', 'conversion', 'conversions', 'traffic', 'optimized', 'optimize', 'domain', 'payable', 'wikipedia'];
+        return ['wordpress', 'woocommerce', 'joomla', 'spa', 'affiliate program', 'prestashop', 'homeopathic', 'incentives', 'deflationary', 'recompound', 'crypto', 'ROI', 'binance', 'reinvestment', 'token', 'financial', 'B2B', 'ecommerce store', 'webmasters', 'nymphomania', 'opencart', 'forex', 'magento', 'bobinternetmarketing', 'business contacts', 'pornography', 'porn', 'capterra', 'dbms', 'whatsapp', 'search engine optimization', 'search results', 'google search', 'guest post', 'social media', 'website traffic', 'no-reply', 'noreply', 'seo', 'smm', 'gay', 'homo', 'homosexual', 'sexual', 'sex', 'bombay', 'mumbai', 'india', 'dating', 'romance', 'babes', 'meet singles', 'earn income', 'earn money', 'xxx', 'marketing', 'blogger', 'article placement', 'service expiration', 'spam', 'medicine', 'human growth hormone', 'life insurance', 'lose weight', 'medicine', 'no medical exams', 'online pharmacy', 'removes wrinkles', 'reverses aging', 'stop snoring', 'valium', 'viagra', 'vicodin', 'weight loss', 'xanax', 'casino', 'paid members', 'lead generation', 'disease', 'romania', 'prostitute', 'mailing list', 'php', 'asp', 'cdb', 'thc', 'delta 8', 'cialis', 'hair loss', 'data recovery', 'nudes', 'hardcore', 'abc', 'url=', 'captcha', 'product key', 'homeopathic', 'nausea', 'remedy', 'remedies', 'online chat', 'chat online', 'sweatheart', 'pakistan', 'href', 'cock', 'suck', 'partnerships', 'lolita', 'fuck', 'loli', 'foto', 'cp', 'pthc', 'clinical', 'cbd', 'cbn', 'pussy', 'masterbate', 'masterbating', 'tits', 'adsense', 'tutorial', 'platform', 'payouts', 'algorithms', 'machine learning', 'machine-learning', 'artificial intelligence', 'ai', 'revenue', 'robot', 'automate', 'backlinks', 'optimization', 'reputation', 'freelancer', 'public relations', 'advertising', 'conversion', 'conversions', 'traffic', 'optimized', 'optimize', 'domain', 'payable', 'wikipedia','telegram','bitcoin','cryptocurrency'];
     }
 
     // Log suspicious request
-    private function logRequest($request)
+    private function logRequest($request, $keyword)
     {
         $sus = new SuspiciousRequest();
         $sus->ip = $request->ip();
@@ -47,7 +47,8 @@ class Keywords
         $sus->headers = $request->header();
         $sus->cookies = $request->cookie();
         $sus->userAgent = $request->useragent();
-        $sus->trigger = get_class($this);
+        $sus->class = get_class($this);
+        $sus->trigger = $keyword;
         $sus->save();
     }
 }
