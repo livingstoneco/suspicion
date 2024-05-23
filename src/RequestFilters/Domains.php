d81f887130cb14a82d79eb7aac97e7ef886d358d
Neil Livingstone <nlivingstone@users.noreply.github.com>
2024-05-23 13:30:58 -0400

    Add new domains

--- src/RequestFilters/Domains.php
+++ src/RequestFilters/Domains.php
@@ -18,7 +18,7 @@ class Domains
     public function handle($request, Closure $next)
     {
         // Loop through request parameters to determine if they contain references to a banned domain
-        foreach ($request->except(['_token']) as $input) {
+        foreach ($request->except(['_token','g-recaptcha-response']) as $input) {
             foreach ($this->domains as $domain) {
                 if (preg_match("/" . preg_quote($domain) . '/mi', $input)) {
                     $this->logRequest($request, $domain);
@@ -33,7 +33,7 @@ class Domains
     // Return array of banned domains
     private function getBannedDomains()
     {
-        return ['test.com', 'test.ca', 'test.net', 'qgp.com', 'test.org', 'mail.ru', 'yandex.com', 'qualityguestposts.com', 'rambler.ru', 'qualitybloggeroutreach.com', 'coldreach.rocks', 'grocket.net', 'mailbanger.com', 'starmedia.ca', 'foxandfigcafe.com', 'migfoam.com', ' buktrk.com', 'freetopfast.com', 'nikitosgross.pw', 'mixfilesmaker.com', 'stancopak.net', 'lone1y.com', 'topworldnewstoday.com', 'jasper-robot.com', 'validbelt.com', 'xtreflectivefilm.com', 'hfxtreflectivefilm.com', 'smartaiwriting.com', 'fiverrseoer.com', 'askgloves.com', 'reputationresults.net', 'fiverr.com', 'bit.ly', 'bitly.com', 'fromfuture.io', ' businesstical.com',' premiumdomainslist.com','wiki-moderator.com','speed-seo.net','t.me','t-online.com','medicopostura.com','socialbusybee.com'];
+        return ['test.com', 'test.ca', 'test.net', 'qgp.com', 'test.org', 'mail.ru', 'yandex.com', 'qualityguestposts.com', 'rambler.ru', 'qualitybloggeroutreach.com', 'coldreach.rocks', 'grocket.net', 'mailbanger.com', 'starmedia.ca', 'foxandfigcafe.com', 'migfoam.com', ' buktrk.com', 'freetopfast.com', 'nikitosgross.pw', 'mixfilesmaker.com', 'stancopak.net', 'lone1y.com', 'topworldnewstoday.com', 'jasper-robot.com', 'validbelt.com', 'xtreflectivefilm.com', 'hfxtreflectivefilm.com', 'smartaiwriting.com', 'fiverrseoer.com', 'askgloves.com', 'reputationresults.net', 'fiverr.com', 'bit.ly', 'bitly.com', 'fromfuture.io', ' businesstical.com',' premiumdomainslist.com','wiki-moderator.com','speed-seo.net','t.me','t-online.com','medicopostura.com','socialbusybee.com','tinyurl.com','form.com'];
     }
 
     // Log suspicious request
