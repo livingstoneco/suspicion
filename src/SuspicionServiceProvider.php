e2fcad006959efa784b6eba0efabeff68f5da4fe
Neil Livingstone <nlivingstone@users.noreply.github.com>
2025-02-20 13:34:17 -0500

    Add return types

--- src/SuspicionServiceProvider.php
+++ src/SuspicionServiceProvider.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Livingstoneco\Suspicion;
 
 use Illuminate\Support\ServiceProvider;
@@ -8,34 +9,40 @@ class SuspicionServiceProvider extends ServiceProvider
     /**
      * Bootstrap the application services.
      */
-    public function boot()
+    public function boot(): void
     {
         $this->loadRoutesFrom(__DIR__ . '/../routes/testing.php');
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
         $this->loadViewsFrom(__DIR__ . '/../resources/views', 'suspicion');
 
         if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__ . '/../config/config.php' => config_path('suspicion.php'),
-            ], 'config');
+            $this->publishes(
+                [
+                    __DIR__ . '/../config/config.php' => config_path('suspicion.php'),
+                ],
+                'config',
+            );
 
-            $this->publishes([
-                __DIR__ . '/../resources/views/errors' => resource_path('views/errors'),
-            ], 'views');
+            $this->publishes(
+                [
+                    __DIR__ . '/../resources/views/errors' => resource_path('views/errors'),
+                ],
+                'views',
+            );
         }
     }
 
     /**
      * Register the application services.
      */
-    public function register()
+    public function register(): void
     {
         // Automatically apply the package configuration
         $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'suspicion');
 
         // Register the main class to use with the facade
         $this->app->singleton('suspicion', function () {
-            return new Suspicion;
+            return new Suspicion();
         });
     }
 }
